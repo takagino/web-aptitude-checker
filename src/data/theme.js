57194426c98f0dad9a9c407bc0b85ad26ceaf92e
takagino <hirotaka.takagi@trident.ac.jp>
2026-02-05 15:14:14 +0900

    0205

--- src/data/theme.js
+++ src/data/theme.js
@@ -2,7 +2,7 @@
 
 // 基本となるカードのスタイル
 export const NEO_CARD =
-  'relative p-6 border-4 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] bg-white relative overflow-hidden';
+  'relative p-6 border-4 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] relative overflow-hidden';
 
 // タイトルなどの装飾ラベル
 export const NEO_LABEL =
